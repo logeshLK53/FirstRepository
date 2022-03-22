97a824472b8f836dd7d919b820db7a47323c0408
logeshLK53 <l.kumar@zensar.com>
2022-03-22 17:41:02 +0530

    assignment

--- Interface/Person.java
+++ Interface/Person.java
@@ -1,8 +1,6 @@
 package Interface;
 
-import java.lang.ref.PhantomReference;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 public class Person implements Comparable<Person>{
