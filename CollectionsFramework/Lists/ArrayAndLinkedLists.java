75952e2171e9103b762baa66b780166072a2a9ce
logeshLK53 <l.kumar@zensar.com>
2022-03-25 12:16:04 +0530

    Collection

---
+++ CollectionsFramework/Lists/ArrayAndLinkedLists.java
@@ -0,0 +1,41 @@
+package CollectionsFramework.Lists;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ArrayAndLinkedLists {
+    public static void main(String[] args) {
+        //Array List
+        ArrayList<Integer> l=new ArrayList<>();
+        l.add(5);
+        l.add(3);
+        l.add(8);
+        System.out.println(l.get(2));
+        l.set(2, 53);
+        System.out.println(l.get(2));
+        System.out.println(l.isEmpty());
+        System.out.println(l.size());
+        System.out.println(l.contains(53));
+        System.out.println(l.indexOf(3));
+        for(int i=0;i<l.size();i++)
+        {
+            System.out.println(l.get(i));
+        }
+//Linked List
+        LinkedList<String> ll=new LinkedList<>();
+        ll.add("Loge");
+        ll.add("Kumar");
+        ll.add("LK");
+        System.out.println(ll.get(2));
+        ll.set(2, "Kowsi");
+        System.out.println(ll.get(2));
+        System.out.println(ll.isEmpty());
+        System.out.println(ll.size());
+        System.out.println(ll.contains("LK"));
+        System.out.println(ll.indexOf("Loge"));
+        for(int i=0;i<ll.size();i++)
+        {
+            System.out.println(ll.get(i));
+        }
+    }
+}
