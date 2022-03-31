a38a092b485d1bcef571a52c0a945fd5739345cb
logeshLK53 <l.kumar@zensar.com>
2022-03-31 16:45:25 +0530

    collections EmpManagement

---
+++ CollectionsFramework/EmployeeManagementA/App.java
@@ -0,0 +1,96 @@
+package CollectionsFramework.EmployeeManagementA;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.UUID;
+
+public class App {
+    static List<EmpModel> list = new ArrayList<>();
+
+    // stati Block -> called before even constructor
+    static {
+        new fdb().loadData(list);
+    }
+    public static EmpModel TakeInfoForEmployee() {
+        Scanner sc = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        
+        System.out.print("Enter Employee Name : ");
+        String name = sc2.nextLine();
+        System.out.print("Enter your salary : ");
+        double salary = sc.nextDouble();
+        System.out.print("Enter your Role : ");
+        String role = sc2.nextLine();
+        System.out.print("Enter your Company's name : ");
+        String company = sc2.nextLine();
+        System.out.print("Enter your total Experince : ");
+        double exp = sc2.nextDouble();
+        System.out.print("Enter your Address : ");
+        String address = sc.nextLine();
+        System.out.print("Enter your Phone number : (+91) ");
+        String phone_no = sc.nextLine();
+        System.out.print("Enter your DOB : ");
+        String dob = sc.nextLine();
+
+        EmpModel emp = new EmpModel(UUID.randomUUID().toString(), extracted(name), salary, extracted(role), extracted(company), exp, extracted(address), extracted(phone_no), extracted(dob));
+        sc.close();
+        sc2.close();
+        return emp;
+    }
+
+    public static String deleteEmployeeById() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Employee Id: ");
+        String id = sc.nextLine();
+
+        sc.close();
+
+        return new fdb().deleteEmployee(extracted(id), list);
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Management System\n");
+        System.out.println("Available Options :-");
+        
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Add an Employee");
+            System.out.println("2. Get all Employee's");
+            System.out.println("3. Delete Employee");
+            System.out.println("4. Exit");
+
+            System.out.print("Enter your choice : ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    EmpModel emp = TakeInfoForEmployee();
+                    System.out.println(new fdb().addEmp(emp, list) + "\n");
+                    break;
+                case 2:
+                    String data = new fdb().getAllData(list);
+                    if(data.isEmpty()) {
+                        System.out.println("No Employee Exist's\n");
+                    } else {
+                        System.out.println(extracted(data) + "\n");
+                    }
+                    break;
+                case 3:
+                    System.out.println(deleteEmployeeById());
+                    break;
+                case 4: 
+                    System.out.println("Thanks for using our Employee System\n");
+                    sc.close();
+                    // Exiting...
+                    System.exit(0);
+                default:
+                    System.out.println("Wrong Choice!\n");
+                    break;
+            }
+        }
+    }
+
+    private static String extracted(String data) {
+        return data;
+    }
+}
---
+++ CollectionsFramework/EmployeeManagementA/fdb.java
@@ -0,0 +1,122 @@
+package CollectionsFramework.EmployeeManagementA;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class fdb {
+    public EmpModel destructureString(List<String> l) {
+        EmpModel model = new EmpModel();
+        for (int i = 0; i < l.size(); i++) {
+            if (i == 0) {
+                model.setAddress(l.get(i).replace("address=", ""));
+            }
+            if (i == 1) {
+                model.setCompany(l.get(i).replace("company=", ""));
+            }
+            if (i == 2) {
+                model.setDob(l.get(i).replace("dob=", ""));
+            }
+            if (i == 3) {
+                model.setExp(Double.parseDouble(l.get(i).replace("exp=", "")));
+            }
+            if (i == 4) {
+                model.setId(l.get(i).replace("id=", ""));
+            }
+            if (i == 5) {
+                model.setName(l.get(i).replace("name=", ""));
+            }
+            if (i == 6) {
+                model.setPhone_no(l.get(i).replace("phone_no=", ""));
+            }
+            if (i == 7) {
+                model.setRole(l.get(i).replace("role=", ""));
+            }
+            if (i == 8) {
+                String salary = l.get(i).replace("salary=", "");
+                salary = salary.substring(0, salary.length() - 1);
+                model.setSalary(Double.parseDouble(salary));
+            }
+        }
+        return model;
+    }
+
+    // Load the Data from fdb.txt
+    public void loadData(List<EmpModel> list) {
+        try {
+            FileReader reader = new FileReader(new File("CollectionsFramework/EmployeeManagementA/fdb.txt"));
+            BufferedReader br = new BufferedReader(reader);
+
+            while (br.read() != -1) {
+                String d = br.readLine();
+                d = d.substring(9, d.length() - 1);
+                EmpModel model = destructureString(Arrays.asList(d.split(", ")));
+
+                list.add(model);
+            }
+            list.forEach(System.out::println);
+            System.out.println("Successfully Loaded Data");
+        } catch (IOException f) {
+            System.out.println(f.getMessage());
+        }
+    }
+
+    // Add Employee to the FDB
+    public String addEmp(EmpModel model, List<EmpModel> list) {
+        File file = new File("CollectionsFramework/EmployeeManagementA/fdb.txt");
+        try {
+            // FileOutputStream fos = new FileOutputStream(file);
+            // ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            // oos.writeObject(model); // Serializing
+
+            // oos.flush();
+            // oos.close();
+
+            list.add(model);
+            FileWriter writer = new FileWriter(file);
+            StringBuffer sb = new StringBuffer();
+
+            for (EmpModel e : list) {
+                sb.append(e.toString() + "\n");
+            }
+            writer.append(sb.toString());
+
+            writer.close();
+            return "Successfully Inserted Data";
+        } catch (IOException e) {
+            return "Error Occured: " + e.getMessage();
+        }
+    }
+
+    // Get All the Data from FDB
+    public String getAllData(List<EmpModel> list) {
+        return list.toString();
+    }
+
+    // Delete Employee by ID
+    public String deleteEmployee(String id, List<EmpModel> list) {
+        try {
+            for (EmpModel e : list) {
+                if (e.getId().equals(id)) {
+                    list.remove(e);
+                    FileWriter writer = new FileWriter(new File("CollectionsFramework/EmployeeManagementA/fdb.txt"));
+                    String data = list.toString();
+                    writer.append(data);
+
+                    writer.close();
+                    return "Deleted Employee with Id=" + id;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return "Id is incorrect";
+
+    }
+}
